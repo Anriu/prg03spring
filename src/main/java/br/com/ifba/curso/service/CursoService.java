2f22771fdd0a9622cc9cd2bc5ad9bebdd3948407
Anriu Oliveira <anriu.oliveira12@gmail.com>
2026-05-16 20:03:08 -0300

    Adição do Spring Data JPA, jutamente com a exclusão do GenericDao assim como todos os Daos

--- src/main/java/br/com/ifba/curso/service/CursoService.java
+++ src/main/java/br/com/ifba/curso/service/CursoService.java
@@ -1,7 +1,7 @@
 package br.com.ifba.curso.service;
 
-import br.com.ifba.curso.dao.CursoIDao;
 import br.com.ifba.curso.entity.Curso;
+import br.com.ifba.curso.repository.CursoRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,37 +13,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class CursoService implements CursoIService {
 
-    // Injeta automaticamente o DAO responsável pelo acesso ao banco
+    // Injeta automaticamente o Repository responsável pelo acesso ao banco
     @Autowired
-    private CursoIDao cursoDao;
+    private CursoRepository cursoRepository;
 
     @Override
     public Curso save(Curso curso) {
         // Salva um novo curso
-        return cursoDao.save(curso);
+        return cursoRepository.save(curso);
     }
 
     @Override
     public Curso update(Curso curso) {
         // Atualiza os dados do curso
-        return cursoDao.update(curso);
+        return cursoRepository.save(curso);
     }
 
     @Override
     public void delete(Curso curso) {
         // Remove um curso
-        cursoDao.delete(curso);
+        cursoRepository.delete(curso);
     }
 
     @Override
     public List<Curso> findAll() {
         // Retorna todos os cursos cadastrados
-        return cursoDao.findAll();
+        return cursoRepository.findAll();
     }
 
     @Override
     public Curso findById(Long id) {
         // Busca um curso pelo ID
-        return cursoDao.findById(id);
+        return cursoRepository.findById(id).orElse(null);
     }
 }
